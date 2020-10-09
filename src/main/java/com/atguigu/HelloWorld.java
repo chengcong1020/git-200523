caeb4d09fe24be238fb9d0ab8182dabc611d1637
cc <cc.atguigu.com>
2020-10-09 22:25:47 +0800

    first commit

---
+++ src/main/java/com/atguigu/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author shkstart
+ * @create 2020-10-09 22:22
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
